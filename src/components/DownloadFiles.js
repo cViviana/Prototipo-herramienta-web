7797c499ff740dce4fde5cac678b05322e7f9008
cViviana <leidyv@unicauca.edu.co>
2024-02-26 22:36:57 -0500

    Changes img DownloadFiles

--- src/components/DownloadFiles.js
+++ src/components/DownloadFiles.js
@@ -1,7 +1,7 @@
 import pdfFile from '../resources/Instrumento de evaluación del conjunto de recomendaciones.pdf';
 import excelFile from '../resources/Instrumento de evaluación.xlsx';
 import { saveAs } from 'file-saver';
-
+import pdfImg from '../../build/pdf.png';
 
 const filename = 'Instrumento de evaluación del conjunto de recomendaciones';
 
@@ -14,11 +14,11 @@ const DownloadFiles = () => {
     return (
       <div className='buttons-download'>
         <button className="button-file" onClick={() => download(pdfFile, filename, 'pdf')}>
-          <img src="https://github.com/cViviana/Prototipo-herramienta-web/blob/008dd6fbb6a3eb9c075a20f6427f20e7634bd943/public/pdf.png" alt="Descargar PDF" />
+          <img src={pdfImg} alt="Descargar PDF" />
           <span>Descargar PDF</span>
         </button>
         <button className="button-file" onClick={() => download(excelFile, filename, 'xlsx')}>
-          <img src="https://github.com/cViviana/Prototipo-herramienta-web/blob/008dd6fbb6a3eb9c075a20f6427f20e7634bd943/public/excel.png" alt="Descargar Excel" />
+          <img src="../../build/excel.png" alt="Descargar Excel" />
           <span>Descargar EXCEL</span>
         </button>
       </div>
