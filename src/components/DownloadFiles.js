c5c448d8a5fc3d1f6c817b989cb45e559bdcb69e
cViviana <leidyv@unicauca.edu.co>
2024-02-26 22:25:05 -0500

    Changes url image to git pages

--- src/components/DownloadFiles.js
+++ src/components/DownloadFiles.js
@@ -2,6 +2,7 @@ import pdfFile from '../resources/Instrumento de evaluación del conjunto de re
 import excelFile from '../resources/Instrumento de evaluación.xlsx';
 import { saveAs } from 'file-saver';
 
+
 const filename = 'Instrumento de evaluación del conjunto de recomendaciones';
 
 function download(auxFile, filename, typeFile) {
@@ -13,11 +14,11 @@ const DownloadFiles = () => {
     return (
       <div className='buttons-download'>
         <button className="button-file" onClick={() => download(pdfFile, filename, 'pdf')}>
-          <img src="pdf.png" alt="Descargar PDF" />
+          <img src="https://github.com/cViviana/Prototipo-herramienta-web/blob/008dd6fbb6a3eb9c075a20f6427f20e7634bd943/public/pdf.png" alt="Descargar PDF" />
           <span>Descargar PDF</span>
         </button>
         <button className="button-file" onClick={() => download(excelFile, filename, 'xlsx')}>
-          <img src="excel.png" alt="Descargar Excel" />
+          <img src="https://github.com/cViviana/Prototipo-herramienta-web/blob/008dd6fbb6a3eb9c075a20f6427f20e7634bd943/public/excel.png" alt="Descargar Excel" />
           <span>Descargar EXCEL</span>
         </button>
       </div>
